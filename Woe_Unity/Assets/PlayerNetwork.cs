d0d27091873db21c301d21edcc547c6b8ffc2133
TrevorGray <graytrevor8@gmail.com>
2023-01-22 20:55:45 -0500

    Get two clients syncing on Unity

---
+++ Woe_Unity/Assets/PlayerNetwork.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (int previousValue, int newValue) => {
+            Debug.Log(OwnerClientId + " - " + randomNumber.Value);
+        };
+    }
+
+    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    // Update is called once per frame
+    void Update() {
+        if (!IsOwner) { return; }
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            randomNumber.Value = Random.Range(0, 10);
+        }
+
+        Vector3 moveDirection = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.W)) {
+            moveDirection.z = +1f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            moveDirection.z = -1f;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            moveDirection.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveDirection.x = +1f;
+        }
+        float moveSpeed = 3.0f;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
