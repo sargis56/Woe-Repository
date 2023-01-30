28260edc0d5d5a82646ba8cf28e16a0204c746c5
TrevorGray <graytrevor8@gmail.com>
2023-01-30 02:05:02 -0500

    Started on my pizzle mechanics

---
+++ Woe_Unity/Assets/MazePuzzle/PillarSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PillarSpawner : MonoBehaviour
+{
+    public GameObject pillarPrehab;
+    public Texture pillarLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int iterator = 0;
+        GameObject current = Instantiate(pillarPrehab, transform.localPosition, transform.rotation, this.transform);
+        current.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = pillarLabel;
+        //iterator++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
