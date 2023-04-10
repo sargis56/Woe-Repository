ed66501199bc1fac2685aaacf577446d28f91055
TrevorGray <graytrevor8@gmail.com>
2023-04-09 22:30:25 -0400

    Added win and lose screen

---
+++ Woe_Unity/Assets/UI/EndScreenMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenMenu : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
